d9f2583c7d088673fcefc7f5ceaa2cff970ce43e
gyuwon09 <133576303+gyuwon09@users.noreply.github.com>
2024-08-03 23:15:35 +0900

    Add bidirectional communication feature

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
--How to setup?
-1. set variables in .properties file
-2. copy webhooker.exe that in dist folder to your server folder with webhooker.properties
-3. launch server to webhooker.exe
+[How to set up?]
+1. Copy the file from the "server pack" folder and paste it into your Minecraft server folder.
+2. Change the variable values in the '.properties' file located in your Minecraft server folder to match your Discord server.
+3. If you want to start your Minecraft server, run 'chatting interlocker.exe' instead of the server launcher.
+
+[warning]
+this program tested just basic Minecraft server. If you using mod server, you should change 'launcher' variable value in 'properties'. If it doesn't work, please enter the command to run your modded Minecraft server launcher in the 'launcher' variable value within the '.properties' file.
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import subprocess
 import re
-import os
 import configparser
+import discord
+import threading
 
 #function for load properties from .properties 
 def load_properties(file_path):
@@ -14,6 +15,7 @@ def load_properties(file_path):
 config = load_properties('.properties')
 WEBHOOK_URL = config['webhook']['url']
 LAUNCHER = config['webhook']['launcher']
+CHANNEL_ID = int(config['webhook']['channel_id'])
 
 #extract player chatting from server log
 def parse_log(log_file):
@@ -46,19 +48,42 @@ def try_decode(output):
             return output.decode('latin-1').strip()
 
 def run_command(cmd):
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    while True:
-        output = process.stdout.readline()
-        if output == b'' and process.poll() is not None:
-            break
-        if output:
-            log_line = try_decode(output)
-            user_id, message = parse_log(log_line)
-            if user_id and message:
-                send_to_discord(user_id, message)
-            else:
-                print(log_line)
-    rc = process.poll()
-    return rc
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
+    
+    def read_output():
+        while True:
+            output = process.stdout.readline()
+            if output == b'' and process.poll() is not None:
+                break
+            if output:
+                log_line = try_decode(output)
+                user_id, message = parse_log(log_line)
+                if user_id and message:
+                    send_to_discord(user_id, message)
+                else:
+                    print(log_line)
 
-run_command(['cmd', '/c', LAUNCHER])
+    threading.Thread(target=read_output, daemon=True).start()
+    return process
+
+def send_command_to_process(process, command):
+    # 명령어를 프로세스의 표준 입력에 전송합니다.
+    process.stdin.write((command + '\n').encode('utf-8'))
+    process.stdin.flush()
+
+
+process = run_command(['cmd', '/c', LAUNCHER])
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'message recognition activated')
+
+    async def on_message(self, message):
+        if message.channel.id == CHANNEL_ID:
+            print(message.content)
+            send_command_to_process(process, f'tellraw @a "<{message.author}> {message.content}"')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run(config['webhook']['token'])
