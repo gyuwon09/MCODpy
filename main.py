3c6dc60833fe5ae6df961aafbbc5a131b5e708d1
gyuwon09 <133576303+gyuwon09@users.noreply.github.com>
2024-08-03 19:31:51 +0900

    change message to embed

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from discord_webhook import DiscordWebhook
+from discord_webhook import DiscordWebhook, DiscordEmbed
 import subprocess
 import re
 import os
 import configparser
 
+#function for load properties from .properties 
 def load_properties(file_path):
     config = configparser.ConfigParser()
     config.read(file_path, encoding='utf-8')
     return config
 
+#load properties
 config = load_properties('.properties')
 WEBHOOK_URL = config['webhook']['url']
 LAUNCHER = config['webhook']['launcher']
 
+#extract player chatting from server log
 def parse_log(log_file):
     match = re.search(r'\[\d{2}:\d{2}:\d{2}\] \[Server thread/INFO\] \[minecraft/MinecraftServer\]: <(.+)> (.+)', log_file)
     if match:
@@ -22,8 +25,14 @@ def parse_log(log_file):
     return None, None
 
 def send_to_discord(user_id, message):
-    webhook = DiscordWebhook(url=WEBHOOK_URL, content=f"**{user_id}**: {message}")
+    webhook = DiscordWebhook(url=WEBHOOK_URL)
+    
+    embed = DiscordEmbed(title=user_id, color='03b2f8')
+    embed.add_embed_field(name='',value=message, inline=False)
+    
+    webhook.add_embed(embed)
     response = webhook.execute()
+    
     if response.status_code != 200:
         print(f"Error sending message to Discord: {response.status_code}, {response.content}")
 
